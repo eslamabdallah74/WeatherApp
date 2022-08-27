679c8079f67d20c8c3ec8ce3da190844268ae6dd
eslamabdallah74 <eslamabdallah301@gmail.com>
2022-08-27 19:03:54 +0200

    change   target

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,7 @@ export default defineNuxtConfig({
   router: {
     base: '/eslamabdallah74/WeatherApp/'
   },
+  target: 'static', // default is 'server'
   publicRuntimeConfig: {
     WEATHER_APP_SECRET: process.env.WEATHER_APP_SECRET,
   },
